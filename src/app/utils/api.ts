6cbe0275fba33aec804b8e4540d9b6ef16f25203
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-07-07 21:50:38 +0900

    動画に付随するプレイリストボタン実行後の処理

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -4,4 +4,10 @@ export const apis = {
   addFavorite: (videoId: string) => `${apiUrl}/api/v1/videos/${videoId}/favorite`,
   removeFavorite: (videoId: string) => `${apiUrl}/api/v1/videos/${videoId}/favorite`,
   checkFavorite: (videoId: string) => `${apiUrl}/api/v1/videos/${videoId}/check`,
+
+  fetchPlaylists: () => `${apiUrl}/api/v1/playlists`,
+  addVideoToPlaylist: (playlistId: string, videoId: string) => 
+    `${apiUrl}/api/v1/playlists/${playlistId}/add_video`,
+  removeVideoFromPlaylist: (playlistId: string, videoId: string) => 
+    `${apiUrl}/api/v1/playlists/${playlistId}/remove_video`,
 };
